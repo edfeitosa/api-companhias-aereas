8c8dfd6da6827187a036664f4e92eb3847ec665f
edfeitosa <eduardofeitosadg@gmail.com>
2022-08-21 23:37:16 -0300

    criação entidade -> aeronave

---
+++ Entities/Aeronave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CiaAerea.Entities
+{
+    public class Aeronave
+    {
+        public int Id { get; set; }
+        public string Fabricante { get; set; }
+        public string Modelo { get; set; }
+        public string Codigo { get; set; }
+
+
+        public Aeronave(string fabricante, string modelo, string codigo)
+        {
+            Fabricante = fabricante;
+            Modelo = modelo;
+            Codigo = codigo;
+        }
+    }
+}
+
