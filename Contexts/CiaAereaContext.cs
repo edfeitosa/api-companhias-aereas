eda9d45cbee36d7a5989e4eefca62a5fed40fda6
edfeitosa <eduardofeitosadg@gmail.com>
2022-08-23 23:47:51 -0300

    criação context -> CiaAerea

---
+++ Contexts/CiaAereaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CiaAerea.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CiaAerea.Contexts
+{
+    public class CiaAereaContext: DbContext
+    {
+        public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
+        public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
+        public DbSet<Manutencao> Manutencoes => Set<Manutencao>();
+        public DbSet<Piloto> Pilotos => Set<Piloto>();
+        public DbSet<Voo> Voos => Set<Voo>();
+
+        public CiaAereaContext()
+        {
+        }
+    }
+}
+
