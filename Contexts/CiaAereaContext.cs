f506a8ee485d6d2dbb0d2d9db6a9b6484916decf
edfeitosa <eduardofeitosadg@gmail.com>
2022-08-24 00:07:30 -0300

    configuração context -> CiaAerea

--- Contexts/CiaAereaContext.cs
+++ Contexts/CiaAereaContext.cs
@@ -6,14 +6,21 @@ namespace CiaAerea.Contexts
 {
     public class CiaAereaContext: DbContext
     {
+        private readonly IConfiguration _configuration;
         public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
         public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
         public DbSet<Manutencao> Manutencoes => Set<Manutencao>();
         public DbSet<Piloto> Pilotos => Set<Piloto>();
         public DbSet<Voo> Voos => Set<Voo>();
 
-        public CiaAereaContext()
+        public CiaAereaContext(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("CiaAerea"));
         }
     }
 }
